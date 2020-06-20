62576063922ec58049fbfde5ef9d1ff02704c39d
--global <ninefeb1995@gmail.com>
2020-06-20 23:31:50 +0700

    project initialization/ [add] single number

---
+++ ClassLibrary/ClassLibrary/SingleNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class SingleNumber
+    {
+        public static int FindSingleNumber(int[] nums)
+        {
+            int result = 0;
+            foreach (var num in nums)
+            {
+                result ^= num;
+            }
+            return result;
+        }
+    }
+}
