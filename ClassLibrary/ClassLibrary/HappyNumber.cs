a53c464e434492ea95fa90e17699988efa036b70
--global <ninefeb1995@gmail.com>
2020-06-24 11:10:30 +0700

    [add] check is happy number or not

---
+++ ClassLibrary/ClassLibrary/HappyNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class HappyNumber
+    {
+        public bool Is(int n)
+        {
+            int result = n;
+
+            while (result != 1 && result != 4)
+            {
+                result = IsHappyNumber(result);
+            }
+
+            return result == 4;
+        }
+
+        private static int IsHappyNumber(int num)
+        {
+            int sum = 0;
+
+            while (num > 0)
+            {
+                int rem = num % 10;
+                sum += rem * rem;
+                num /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
