981240cef665bd6a709ba3b65592d83256c14bc9
--global <ninefeb1995@gmail.com>
2020-06-26 14:41:56 +0700

    [add] Middle of the Linked List

---
+++ ClassLibrary/ClassLibrary/MiddleLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class MiddleLinkedList
+    {
+        public static ListNode FindMiddleOfLinkedList(ListNode head)
+        {
+            var i = 1;
+            var temp = head;
+            var listNodeInList = new List<ListNode>() { temp };
+            do
+            {
+                temp = temp.next;
+                if (temp == null)
+                {
+                    break;
+                }
+                i++;
+                listNodeInList.Add(temp);
+            } while (true);
+
+            return listNodeInList[(i / 2)];
+        }
+
+        public static ListNode FindMiddleOfLinkedList1(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast?.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next?.next;
+            }
+
+            return slow;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
--- ClassLibrary/ClassLibrary/SingleNumber.cs
+++ ClassLibrary/ClassLibrary/SingleNumber.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 
 namespace ClassLibrary
 {
@@ -12,6 +12,32 @@ namespace ClassLibrary
                 result ^= num;
             }
             return result;
+        }
+
+        public static int FindSingleNumber1(int[] nums)
+        {
+            var numInList = nums.ToList();
+            int i;
+            for (i = 0; i < numInList.Count; i++)
+            {
+                int j;
+                for (j = i + 1; j < numInList.Count; j++)
+                {
+                    if (numInList[i] == numInList[j])
+                    {
+                        break;
+                    }
+                }
+                if (j == numInList.Count)
+                {
+                    break;
+                }
+                else
+                {
+                    numInList.RemoveAt(j);
+                }
+            }
+            return numInList[i];
         }
     }
 }
